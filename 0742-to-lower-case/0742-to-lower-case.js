c97ada54f2d059208a18096867c9dad4d9c6f5a8
rizwanmuhammedd <risvanmd172@gmail.com>
2025-08-16 20:03:36 +0530

    Time: 1 ms (15.47%), Space: 53.6 MB (51.36%) - LeetHub

---
+++ 0742-to-lower-case/0742-to-lower-case.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var toLowerCase = function(s) {
+    let string=""
+    for(i=0;i<s.length;i++){
+        string=s.toLowerCase()
+    }return string;
+};
