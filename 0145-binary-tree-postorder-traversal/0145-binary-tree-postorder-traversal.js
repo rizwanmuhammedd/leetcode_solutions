85e7e8fae6d1f6ce697872d4e7b858a2baf29a09
rizwanmuhammedd <risvanmd172@gmail.com>
2025-10-14 09:38:24 +0530

    Time: 0 ms (100%), Space: 54.1 MB (48.26%) - LeetHub

---
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var postorderTraversal = function(root) {
+
+    if (!root) return [];
+
+    const stack = [root];
+    const output = [];
+
+    while (stack.length) {
+        const node = stack.pop();
+        output.push(node.val);
+
+        if (node.left) stack.push(node.left);
+        if (node.right) stack.push(node.right);
+    }
+
+    return output.reverse(); 
+};
