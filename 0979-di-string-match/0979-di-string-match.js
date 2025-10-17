eeac840dc16e71b8b8c4fc4a97b574dd05a3fbb7
rizwanmuhammedd <risvanmd172@gmail.com>
2025-10-17 22:23:34 +0530

    Time: 5 ms (41.4%), Space: 57.9 MB (81.18%) - LeetHub

---
+++ 0979-di-string-match/0979-di-string-match.js
@@ -0,0 +1,14 @@
+var diStringMatch = function(s) {
+  let low = 0;
+  let high = s.length;
+  let result = [];
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "I") result.push(low++);
+    else result.push(high--);
+  }
+
+  result.push(low);
+
+  return result;
+};
